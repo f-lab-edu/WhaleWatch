1cbe9e6e60ae64ca501e4042674200acdc8d494e
Junmo <hongjm1022@gmail.com>
2024-12-14 16:36:39 +0900

    feat: PostController 구조체로변경

--- api/src/main/java/com/whalewatch/controller/PostController.java
+++ api/src/main/java/com/whalewatch/controller/PostController.java
@@ -3,23 +3,28 @@ package com.whalewatch.controller;
 import com.whalewatch.common.dto.PostDto;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
+    private List<PostDto> posts = new ArrayList<>();
+
+    public PostController(){
+        posts.add(new PostDto(1,"Test1","Test1"));
+        posts.add(new PostDto(2,"Test2","Test2"));
+    }
 
     @GetMapping
     public List<PostDto> getPosts(){
-        return Arrays.asList(
-                new PostDto(1,"Post1","post1"),
-                new PostDto(2,"Post2","post2")
-        );
+        return posts;
     }
 
     @PostMapping
-    public String createPost(@RequestBody PostDto post) {
-        return "게시글 생성 : " + post.getTitle();
+    public PostDto createPost(@RequestBody PostDto post) {
+        posts.add(post);
+        return post;
     }
 }
