493c0c23aba41936d0fd1d8e58b85e2fc5f95af8
Junmo <hongjm1022@gmail.com>
2025-01-06 16:58:13 +0900

    Feat: JWT service 생성

--- api/src/main/java/com/whalewatch/controller/UserController.java
+++ api/src/main/java/com/whalewatch/controller/UserController.java
@@ -1,1 +1,49 @@
- 
+package com.whalewatch.controller;
+
+import com.whalewatch.domain.User;
+import com.whalewatch.dto.TokenResponseDto;
+import com.whalewatch.dto.UserDto;
+import com.whalewatch.mapper.UserMapper;
+import com.whalewatch.service.JwtService;
+import com.whalewatch.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+    private final JwtService jwtService;
+    private final UserMapper userMapper;
+
+    public UserController(UserService userService,
+                          UserMapper userMapper,
+                          JwtService jwtService) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping
+    public UserDto registerUser(@RequestBody UserDto userDto) {
+        User entity = userMapper.toEntity(userDto);
+        User saved = userService.registerUser(entity);
+        return userMapper.toDto(saved);
+    }
+
+    @PostMapping("/login")
+    public TokenResponseDto loginUser(@RequestBody UserDto userDto) {
+        // JwtService로 로그인 + 토큰 발급
+        return jwtService.login(userDto.getEmail(),userDto.getPassword());
+    }
+
+    @PostMapping("/refresh")
+    public TokenResponseDto refreshToken(@RequestBody TokenResponseDto tokenDto){
+        return jwtService.refreshAccessToken(tokenDto.getRefreshToken());
+    }
+
+    @GetMapping("{id}")
+    public UserDto getUserInfo(@PathVariable int id) {
+        User user = userService.getUserInfo(id);
+        return userMapper.toDto(user);
+    }
+}
--- service/src/main/java/com/whalewatch/service/UserService.java
+++ service/src/main/java/com/whalewatch/service/UserService.java
@@ -2,17 +2,24 @@ package com.whalewatch.service;
 
 import com.whalewatch.domain.User;
 import com.whalewatch.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User registerUser(User user) {
+        String hashed = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashed);
+
         return userRepository.save(user);
     }
 
