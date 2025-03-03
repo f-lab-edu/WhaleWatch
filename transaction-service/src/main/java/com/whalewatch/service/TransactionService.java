d59f25f636e7dc73a64685e88aa731bb5e7a6920
Junmo <hongjm1022@gmail.com>
2025-03-03 19:37:42 +0900

    refactor: Transactionservice kafka change

--- transaction-service/src/main/java/com/whalewatch/service/TransactionService.java
+++ transaction-service/src/main/java/com/whalewatch/service/TransactionService.java
@@ -2,9 +2,7 @@ package com.whalewatch.service;
 
 import com.whalewatch.domain.Transaction;
 import com.whalewatch.dto.TransactionEventDto;
-import com.whalewatch.repository.AlertRepository;
 import com.whalewatch.repository.TransactionRepository;
-import com.whalewatch.service.UserAlertService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -17,19 +15,13 @@ public class TransactionService {
     private static final Logger log = LoggerFactory.getLogger(TransactionService.class);
 
     private final TransactionRepository transactionRepository;
-    private final AlertRepository alertRepository;
-    private final UserAlertService userAlertService;
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     private final String TRANSACTION_EVENT = "transaction_event";
 
     public TransactionService(TransactionRepository transactionRepository,
-                              AlertRepository alertRepository,
-                              UserAlertService userAlertService,
                               KafkaTemplate<String, Object> kafkaTemplate) {
         this.transactionRepository = transactionRepository;
-        this.alertRepository = alertRepository;
-        this.userAlertService = userAlertService;
         this.kafkaTemplate = kafkaTemplate;
     }
 
