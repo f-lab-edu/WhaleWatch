4aedf7f2de22fc9a1629f2c58971a44c2baf233f
Junmo <hongjm1022@gmail.com>
2025-04-20 16:54:35 +0900

    Readme update

--- post-service/bin/generated-sources/annotations/com/whalewatch/mapper/PostMapperImpl.java
+++ post-service/bin/generated-sources/annotations/com/whalewatch/mapper/PostMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-09T19:08:01+0900",
-    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.41.0.z20250213-2037, environment: Java 21.0.6 (Eclipse Adoptium)"
+    date = "2025-04-20T16:47:07+0900",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
 public class PostMapperImpl implements PostMapper {
